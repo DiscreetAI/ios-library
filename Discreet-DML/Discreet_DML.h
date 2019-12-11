561f344d4ab8146c7ea693150fb258a8251009ea
neeleshdodda44 <ndodda@berkeley.edu>
2019-12-10 20:16:12 -0800

    adding initial files

---
+++ Discreet-DML/Discreet_DML.h
@@ -0,0 +1,19 @@
+//
+//  Discreet_DML.h
+//  Discreet-DML
+//
+//  Created by Neelesh on 12/10/19.
+//  Copyright © 2019 DiscreetAI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Discreet_DML.
+FOUNDATION_EXPORT double Discreet_DMLVersionNumber;
+
+//! Project version string for Discreet_DML.
+FOUNDATION_EXPORT const unsigned char Discreet_DMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Discreet_DML/PublicHeader.h>
+
+

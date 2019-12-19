d5a238a69b9f6a4d74f88be0bb471df1a3664f0f
neeleshdodda44 <ndodda@berkeley.edu>
2019-12-18 17:58:42 -0800

    adding initial class

---
+++ Discreet-DML/WeightsProcessor.swift
@@ -0,0 +1,13 @@
+//
+//  WeightsProcessor.swift
+//  Discreet-DML
+//
+//  Created by Neelesh on 12/18/19.
+//  Copyright © 2019 DiscreetAI. All rights reserved.
+//
+
+import Foundation
+
+class WeightsProcessor {
+    
+}

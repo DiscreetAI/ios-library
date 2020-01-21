3fd9d978314395c2e86f4535c2dc8d526503d2ea
neeleshdodda44 <ndodda@berkeley.edu>
2020-01-20 19:49:19 -0800

    adding mpsutils file

---
+++ Discreet-DML/MPSUtils.swift
@@ -0,0 +1,12 @@
+//
+//  MPSUtils.swift
+//  Discreet-DML
+//
+//  Created by Neelesh on 1/20/20.
+//  Copyright © 2020 DiscreetAI. All rights reserved.
+//
+
+import Foundation
+import MetalPerformanceShaders
+
+

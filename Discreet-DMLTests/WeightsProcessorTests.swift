8c58d82ba1f7d0f4f242287db83dc2023483fd82
neeleshdodda44 <ndodda@berkeley.edu>
2020-01-27 14:36:22 -0800

    refactor tests

--- Discreet-DMLTests/WeightsProcessorTests.swift
+++ Discreet-DMLTests/WeightsProcessorTests.swift
@@ -11,8 +11,14 @@ import XCTest
 import CoreML
 @testable import Discreet_DML
 
+
+class DummyMPSHandler : MPSHandler {
+    override init() {
+        
+    }
+}
+
 class WeightsProcessorTests: XCTestCase {
-    var weightsProcessor = WeightsProcessor()
 
     // Folder to testing artifacts
     let artifactsPath: String = testingUtilsPath + "WeightsProcessor/"
@@ -24,6 +30,7 @@ class WeightsProcessorTests: XCTestCase {
         /*
          Test gradient calculation for a model with one layer.
          */
+        var weightsProcessor = WeightsProcessor(mpsHandler: DummyMPSHandler())
         let oldSimpleWeightsPath: String = artifactsPath + "old_simple_weights"
         let newSimpleWeightsPath: String = artifactsPath + "new_simple_weights"
         let calculatedGradients: [[Float32]] = weightsProcessor.calculateGradients(oldModelPath: oldSimpleWeightsPath, newModelPath: newSimpleWeightsPath, learningRate: 0.01, useGPU: false)
@@ -36,6 +43,7 @@ class WeightsProcessorTests: XCTestCase {
         /*
          Test gradient calculation for a model with multiple layers.
          */
+        var weightsProcessor = WeightsProcessor(mpsHandler: DummyMPSHandler())
         let oldComplexWeightsPath: String = artifactsPath + "old_complex_weights"
         let newComplexWeightsPath: String = artifactsPath + "new_complex_weights"
         let calculatedGradients: [[Float32]] = weightsProcessor.calculateGradients(oldModelPath: oldComplexWeightsPath, newModelPath: newComplexWeightsPath, learningRate: 0.01, useGPU: false)
@@ -54,6 +62,7 @@ class WeightsProcessorTests: XCTestCase {
          */
         #if targetEnvironment(simulator)
         #else
+        var weightsProcessor = WeightsProcessor(mpsHandler: MPSHandler())
         let oldSimpleWeightsPath: String = artifactsPath + "old_simple_weights"
         let newSimpleWeightsPath: String = artifactsPath + "new_simple_weights"
         let calculatedGradients: [[Float32]] = weightsProcessor.calculateGradients(oldModelPath: oldSimpleWeightsPath, newModelPath: newSimpleWeightsPath, learningRate: 0.01)
@@ -71,6 +80,7 @@ class WeightsProcessorTests: XCTestCase {
          */
         #if targetEnvironment(simulator)
         #else
+        var weightsProcessor = WeightsProcessor(mpsHandler: MPSHandler())
         let oldComplexWeightsPath: String = artifactsPath + "old_complex_weights"
         let newComplexWeightsPath: String = artifactsPath + "new_complex_weights"
         let calculatedGradients: [[Float32]] = weightsProcessor.calculateGradients(oldModelPath: oldComplexWeightsPath, newModelPath: newComplexWeightsPath, learningRate: 0.01)

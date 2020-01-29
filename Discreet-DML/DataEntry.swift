3be9bb134d3c06d4b1409acd7bf57f24017a6490
neeleshdodda44 <ndodda@berkeley.edu>
2020-01-28 18:39:49 -0800

    travis sucks, reformatting

--- Discreet-DML/DataEntry.swift
+++ Discreet-DML/DataEntry.swift
@@ -1,61 +1,73 @@
-////
-////  DataEntry.swift
-////  Discreet-DML
-////
-////  Created by Neelesh on 1/27/20.
-////  Copyright © 2020 DiscreetAI. All rights reserved.
-////
-//import Foundation
-//import RealmSwift
 //
-//public class DataEntry: Object {
-//    @objc dynamic var repoID: String = ""
+//  DataEntry.swift
+//  Discreet-DML
 //
-//    convenience init(repoID: String) {
-//        self.init()
-//        self.repoID = repoID
-//    }
+//  Created by Neelesh on 1/27/20.
+//  Copyright © 2020 DiscreetAI. All rights reserved.
 //
-//    public override static func primaryKey() -> String? {
-//        return "repoID"
-//    }
-//}
-//
-//public class ImageEntry: DataEntry {
-//    var images: List<String> = List<String>()
-//
-//    convenience init(repoID: String, images: [String]) {
-//        self.init(repoID: repoID)
-//        addImages(images: images)
-//    }
-//
-//    func addImages(images: [String]) {
-//        self.images.append(objectsIn: images)
-//    }
-//}
-//
-//public class DoubleEntry: DataEntry {
-//    var data: List<DoubleDatapoint> = List<DoubleDatapoint>()
-//
-//    convenience init(repoID: String, datapoints: [[Double]]) {
-//        self.init(repoID: repoID)
-//        addData(datapoints: datapoints)
-//    }
-//
-//    func addData(datapoints: [[Double]]) {
-//        let doubleDatapoints = datapoints.map({
-//            (datapoint: [Double]) -> DoubleDatapoint in
-//            return DoubleDatapoint(datapoint: datapoint)
-//        })
-//        self.data.append(objectsIn: doubleDatapoints)
-//    }
-//}
-//
-//public class DoubleDatapoint: Object {
-//    let datapoint: List<Double> = List<Double>()
-//
-//    convenience init(datapoint: [Double]) {
-//        self.init()
-//        self.datapoint.append(objectsIn: datapoint)
-//    }
-//}
+import Foundation
+import RealmSwift
+
+public class DataEntry: Object {
+    /*
+     General dataset object. Uniquely identified by `repoID`.
+     */
+    @objc dynamic var repoID: String = ""
+
+    convenience init(repoID: String) {
+        self.init()
+        self.repoID = repoID
+    }
+
+    public override static func primaryKey() -> String? {
+        return "repoID"
+    }
+}
+
+public class ImageEntry: DataEntry {
+    /*
+     Dataset object representing a list of paths to images on device.
+     */
+    var images: List<String> = List<String>()
+
+    convenience init(repoID: String, images: [String]) {
+        self.init(repoID: repoID)
+        addImages(images: images)
+    }
+
+    func addImages(images: [String]) {
+        self.images.append(objectsIn: images)
+    }
+}
+
+public class DoubleEntry: DataEntry {
+    /*
+     Dataset object representing a 2D array of Doubles.
+     */
+    var data: List<DoubleDatapoint> = List<DoubleDatapoint>()
+
+    convenience init(repoID: String, datapoints: [[Double]]) {
+        self.init(repoID: repoID)
+        addData(datapoints: datapoints)
+    }
+
+    func addData(datapoints: [[Double]]) {
+        let doubleDatapoints = datapoints.map({
+            (datapoint: [Double]) -> DoubleDatapoint in
+            return DoubleDatapoint(datapoint: datapoint)
+        })
+        self.data.append(objectsIn: doubleDatapoints)
+    }
+}
+
+public class DoubleDatapoint: Object {
+    /*
+     Datapoint object representing a 1D array of Doubles.
+     */
+    let datapoint: List<Double> = List<Double>()
+
+    convenience init(datapoint: [Double]) {
+        self.init()
+        self.datapoint.append(objectsIn: datapoint)
+    }
+}
--- Discreet-DMLTests/RealmClientTests.swift
+++ Discreet-DMLTests/RealmClientTests.swift
@@ -1,50 +1,43 @@
-////
-////  RealmClientTests.swift
-////  Discreet-DMLTests
-////
-////  Created by Neelesh on 1/27/20.
-////  Copyright © 2020 DiscreetAI. All rights reserved.
-////
+//
+//  RealmClientTests.swift
+//  Discreet-DMLTests
+//
+//  Created by Neelesh on 1/27/20.
+//  Copyright © 2020 DiscreetAI. All rights reserved.
+//
 import XCTest
 import RealmSwift
 @testable import Discreet_DML
 
 class RealmClientTests: XCTestCase {
+    var realmClient = RealmClient()
     
-//
-//    override func setUp() {
-//        realmClient.clear()
-//        // Put setup code here. This method is called before the invocation of each test method in the class.
-//    }
-//
-//    override func tearDown() {
-//        //realmClient.clear()
-//        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//    }
-//
-//    func testRealmGetStore() {
-//        // This is an example of a functional test case.
-//        // Use XCTAssert and related functions to verify your tests produce the correct results.
-//        let data: [[Double]] = [[1.1, 2.2], [3.3, 4.4]]
-//        realmClient.storeData(repoID: "test", data: data)
-//        let result: DoubleEntry? = realmClient.getDoubleEntry(repoID: "test")
-//        XCTAssertNotNil(result)
-//        if result != nil {
-//            let resultData = Array(result!.data).map({
-//                (datapoint: DoubleDatapoint) -> [Double] in
-//                return Array(datapoint.datapoint)
-//            })
-//            XCTAssertEqual(data, resultData)
-//        }
-//
-//    }
-//
-//    func testBadGet() {
-//        var result = realmClient.getDataEntry(repoID: "test")
-//        XCTAssertNil(result)
-//    }
-    func testSanity() {
-        var realmClient: RealmClient = RealmClient()
-        XCTAssertNil(nil)
+    override func setUp() {
+        realmClient.clear()
+    }
+    
+    func testRealmGetStore() {
+        /*
+         Test storing data and retrieving it.
+         */
+        let data: [[Double]] = [[1.1, 2.2], [3.3, 4.4]]
+        realmClient.storeData(repoID: "test", data: data)
+        let result: DoubleEntry? = realmClient.getDoubleEntry(repoID: "test")
+        XCTAssertNotNil(result)
+        if result != nil {
+            let resultData = Array(result!.data).map({
+                (datapoint: DoubleDatapoint) -> [Double] in
+                return Array(datapoint.datapoint)
+            })
+            XCTAssertEqual(data, resultData)
+        }
+    }
+
+    func testBadGet() {
+        /*
+         Test getting with an invalid `repoID`.
+         */
+        var result = realmClient.getDataEntry(repoID: "test")
+        XCTAssertNil(result)
     }
 }

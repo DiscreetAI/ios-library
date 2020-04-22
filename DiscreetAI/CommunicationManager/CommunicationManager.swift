2cb17b1ce21e5a5b1f2a355be5d994b5bf1ab098
neeleshdodda44 <ndodda@berkeley.edu>
2020-04-22 11:35:16 -0700

    Updates for CocoaPod Release

--- DiscreetAI/CommunicationManager/CommunicationManager.swift
+++ DiscreetAI/CommunicationManager/CommunicationManager.swift
@@ -241,7 +241,7 @@ class CommunicationManager: WebSocketDelegate {
         self.reconnections -= 1
         if self.reconnections > 0 {
             print("Reconnecting...")
-            connect()
+            _ = connect()
         } else {
             throw DMLError.communicationManagerError(ErrorMessage.failedConnection)
         }
